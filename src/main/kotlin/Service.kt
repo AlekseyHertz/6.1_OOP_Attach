c52b5cb0ec8c675f4b6dd7d295894050c49e2f5f
Aleksey_Hertz <TihAleksey@rambler.ru>
2023-04-20 14:39:29 +0500

    fix 6.OOP

---
+++ src/main/kotlin/Service.kt
@@ -0,0 +1,11 @@
+data class Likes(
+    var countLike: Int, // число лайков
+    val canLike: Boolean,// возможность "лайкать"
+)
+
+data class Comment(
+    var countComment: Int, // количество комментарий
+    val canCommentPost: Boolean, // возможность комментирования
+    val canCommentOpen: Boolean, // возможность просмотра
+    val textComment: String, //текст комментария
+)
